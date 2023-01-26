48c01d4088c5aa9d6c24a462b66b242b3c35b4f0
VaKo2179 <115894285+VaKo2179@users.noreply.github.com>
2023-01-26 17:11:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ with open("numbers.txt", "r") as f:
 # Append the maximum number to the file
 with open("numbers.txt", "w") as f:
     f.write (str(max_number))
+    
+# task 2
+#----------
+def min_value(dictionary):
+    return min(dictionary.values())
 
+# task3
+#---------
+def unique_set(file_name):
+    set1 = set()
+    with open(file_name) as file:
+        lines = file.readlines()
+        for line in lines:
+            set1.add(line)
+
+    return set1
 
